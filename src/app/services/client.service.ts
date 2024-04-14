d97f00299f95a7bb93d66e3afb4a14166cc0720a
mahmouddhamdy <68087837+mahmouddhamdy@users.noreply.github.com>
2024-04-14 23:57:26 +0200

    Update payment

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -67,4 +67,8 @@ export class ClientService {
         })
       );
   }
+
+  completeOrder(client:any,freelancerID:any){
+    return this.http.post(this.DB_URL+"completeOrder",{client:client,freelancerID:freelancerID});
+  }
 }
